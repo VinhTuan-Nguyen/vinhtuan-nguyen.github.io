3f06cbd5404071c87f2ab2cff8c601b4088cce7b
Tuan Nguyen Vinh <vinhtuanbm@gmai.com>
2025-06-04 21:44:47 +0700

    update code

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -29,7 +29,7 @@ const ContactForm: React.FC = () => {
       setFormStatus(null)
       setProcessStatus('start')
 
-      await fetch(`https://send-mail-six-pi.vercel.app/send-mail`, {
+      await fetch(`https://send-mail-umber-six.vercel.app/send-mail`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
